00e1355f3f0a789fc90fbe0eb4a8f9ff4e2b6c86
zhangliyong <zhangliyong@umeng.com>
2014-08-05 15:05:43 +0800

    Add long_description to setup

--- setup.py
+++ setup.py
@@ -1,8 +1,32 @@
+"""
+rq-gevent-worker
+================
+
+Implement a new worker based on gevent
+
+Install
+-------
+
+.. code-block:: bash
+
+    $ pip install rq-gevent-worker
+
+Usage
+-----
+
+.. code-block:: bash
+
+    $ rqgeventworker -h
+
+    $ export PYTHONPATH=<your project import path>:$PYTHONPATH; rqgeventworker
+
+For more information: https://github.com/zhangliyong/rq-gevent-worker
+"""
 from setuptools import setup
 
 setup(
     name='rq-gevent-worker',
-    version='0.1.3',
+    version='0.1.4',
     py_modules=['rq_gevent_worker'],
     entry_points={
         'console_scripts': [
@@ -14,5 +38,6 @@ setup(
     author='Lyon Zhang',
     author_email='lyzhang87@gmail.com',
     description='Implement a new worker based on gevent',
+    long_description=__doc__,
     install_requires=['rq >= 0.4.6', 'gevent >= 1.0'],
 )
